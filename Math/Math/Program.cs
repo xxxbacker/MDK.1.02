038d46d21799e06b546c047f8d83238a6f58c4c1
xxxbacker <116801044+xxxbacker@users.noreply.github.com>
2023-05-30 15:03:32 +0300

    Add files via upload

---
+++ Math/Math/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Math1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(100));
+            Console.WriteLine(Math.Max(100, 250));
+            Console.WriteLine(Math.Sqrt(100));
+            Console.WriteLine(Math.Truncate(32.7865m));
+            //индивидуальное задание 
+            //вариант 26
+        }
+    }
+}
---
+++ mathdrob/mathdrob/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine("введите 2 числа: x, y");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+
+double st1;
+double st2;
+double st3;
+double a;
+
+st1 = Math.Pow(y, 2)+Math.Sin(x)/Math.Tan(y)+Math.Cos(x);
+st2 = Math.Pow(y,3)+Math.Sin(x)/1+Math.Pow(Math.Cos(x*x),2);
+st3 = Math.Pow(y, 4) + Math.Sin(x) / Math.Pow(Math.Tan(y), -1) + Math.Pow(Math.Cos(x * x * x), 3);
+
+a= st1 - st2 + st3;
+Console.WriteLine(a);
